c3641056996d2892add78dc2ef045706ce82f853
Eneko Etxaniz Monge <79299525+Etxaniz100@users.noreply.github.com>
2026-07-27 13:03:38 +0200

    Simple test, delete all and make a simplified test
    Followed guide:
    https://bengsfort.github.io/articles/making-a-js-game-part-1-game-engine/

---
+++ js/game.js
@@ -0,0 +1,36 @@
+var cUtils = require('./utils/utils.canvas.js'),
+    $container = document.getElementById('container');
+
+function Game(w, h, targetFps, showFps) {
+    // Setup some constants
+    this.constants = {
+        width: w,
+        height: h,
+        targetFps: targetFps,
+        showFps: showFps
+    };
+
+       this.state = {};
+
+  // Generate a canvas and store it as our viewport
+  this.viewport = cUtils.generateCanvas(w, h);
+  this.viewport.id = "gameViewport"; // give the canvas an ID for easy CSS/JS targeting
+
+  // Get and store the canvas context as a global
+  this.context = this.viewport.getContext('2d');
+
+  // Append our viewport into a container in the dom
+  $container.insertBefore(this.viewport, $container.firstChild);
+
+  // Spit out some text
+  this.context.font = '62px Arial';
+  this.context.fillStyle = '#e0c5c5';
+  this.context.fillText('It\'s dangerous to travel this route alone.', 5, 50);
+
+  return this;
+}
+
+// Instantiate a new game in the global scope at 800px by 600px
+window.game = new Game(800, 600, 60, true);
+
+module.exports = game;
